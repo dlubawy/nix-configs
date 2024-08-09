8caee21b045262688afb5834658febabc2e3a8cf
Andrew Lubawy <andrew@andrewlubawy.com>
2024-08-08 23:55:56 -0700

    Migrate adguardhome config to import and add personalized defaults

---
+++ hosts/pi/adguardhome.nix
@@ -0,0 +1,131 @@
+{
+  lib,
+  config,
+  pkgs,
+  ...
+}:
+{
+  age = {
+    secrets = {
+      adguardhome = {
+        file = ../../secrets/adguardhome.age;
+        mode = "0444";
+      };
+    };
+  };
+
+  systemd.services.adguardhome.preStart =
+    let
+      cfg = config.services.adguardhome;
+      settingsFormat = pkgs.formats.yaml { };
+      settings =
+        if (cfg.settings != null) then
+          cfg.settings
+          // (
+            if cfg.settings.schema_version < 23 then
+              {
+                bind_host = cfg.host;
+                bind_port = cfg.port;
+              }
+            else
+              { http.address = "${cfg.host}:${toString cfg.port}"; }
+          )
+        else
+          null;
+      configFile = (settingsFormat.generate "AdGuardHome.yaml" settings).overrideAttrs (_: {
+        checkPhase = "${cfg.package}/bin/adguardhome -c $out --check-config";
+      });
+    in
+    lib.mkForce (
+      lib.optionalString (settings != null) ''
+        if    [ -e "$STATE_DIRECTORY/AdGuardHome.yaml" ] \
+           && [ "${toString cfg.mutableSettings}" = "1" ]; then
+          # Writing directly to AdGuardHome.yaml results in empty file
+          ${pkgs.yaml-merge}/bin/yaml-merge "$STATE_DIRECTORY/AdGuardHome.yaml" "${configFile}" "${config.age.secrets.adguardhome.path}" > "$STATE_DIRECTORY/AdGuardHome.yaml.tmp"
+          mv "$STATE_DIRECTORY/AdGuardHome.yaml.tmp" "$STATE_DIRECTORY/AdGuardHome.yaml"
+        else
+          cp --force "${configFile}" "$STATE_DIRECTORY/AdGuardHome.yaml"
+          chmod 600 "$STATE_DIRECTORY/AdGuardHome.yaml"
+        fi
+      ''
+    );
+
+  services = {
+    nginx = {
+      enable = true;
+      virtualHosts = {
+        "adguard.home" = {
+          locations."/" = {
+            proxyPass = "http://localhost:3000";
+          };
+        };
+      };
+    };
+
+    adguardhome = {
+      enable = true;
+      host = "127.0.0.1";
+      port = 3000;
+      settings = {
+        users = [
+          {
+            name = "admin";
+            password = "@adguardhome-password@";
+          }
+        ];
+        dns = {
+          upstream_dns = [
+            "https://security.cloudflare-dns.com/dns-query"
+            "https://dns.quad9.net/dns-query"
+          ];
+          bootstrap_dns = [
+            "8.8.8.8"
+            "8.8.4.4"
+            "2001:4860:4860::8888"
+            "2001:4860:4860::8844"
+          ];
+        };
+        filters = [
+          {
+            enabled = true;
+            url = "https://adguardteam.github.io/HostlistsRegistry/assets/filter_1.txt";
+            name = "AdGuard DNS filter";
+            id = 1;
+          }
+          {
+            enabled = false;
+            url = "https://adguardteam.github.io/HostlistsRegistry/assets/filter_2.txt";
+            name = "AdAway Default Blocklist";
+            id = 2;
+          }
+          {
+            enabled = true;
+            url = "https://adguardteam.github.io/HostlistsRegistry/assets/filter_7.txt";
+            name = "Perflyst and Dandelion Sprout's Smart-TV Blocklist";
+            id = 1723182134;
+          }
+          {
+            enabled = true;
+            url = "https://adguardteam.github.io/HostlistsRegistry/assets/filter_48.txt";
+            name = "HaGeZi's Pro Blocklist";
+            id = 1723182135;
+          }
+          {
+            enabled = true;
+            url = "https://adguardteam.github.io/HostlistsRegistry/assets/filter_44.txt";
+            name = "HaGeZi's Threat Intelligence Feeds";
+            id = 1723182136;
+          }
+        ];
+        filtering = {
+          rewrites = [
+            {
+              domain = "adguard.home";
+              answer = "192.168.0.34";
+            }
+          ];
+        };
+      };
+    };
+  };
+}
--- hosts/pi/default.nix
+++ hosts/pi/default.nix
@@ -1,7 +1,4 @@
 {
-  lib,
-  config,
-  pkgs,
   inputs,
   outputs,
   vars,
@@ -12,6 +9,7 @@
     inputs.agenix.nixosModules.default
     ../../nixos
     ./hardware-configuration.nix
+    ./adguardhome.nix
   ];
 
   environment.shellAliases = {
@@ -51,73 +49,5 @@
         KbdInteractiveAuthentication = false;
       };
     };
-    adguardhome = {
-      enable = true;
-      host = "127.0.0.1";
-      port = 3000;
-      settings = {
-        users = [
-          {
-            name = "admin";
-            password = "@adguardhome-password@";
-          }
-        ];
-      };
-    };
-    nginx = {
-      enable = true;
-      virtualHosts = {
-        "adguard.home" = {
-          locations."/" = {
-            proxyPass = "http://localhost:3000";
-          };
-        };
-      };
-    };
   };
-
-  age = {
-    secrets = {
-      adguardhome = {
-        file = ../../secrets/adguardhome.age;
-        mode = "0444";
-      };
-    };
-  };
-
-  systemd.services.adguardhome.preStart =
-    let
-      cfg = config.services.adguardhome;
-      settingsFormat = pkgs.formats.yaml { };
-      settings =
-        if (cfg.settings != null) then
-          cfg.settings
-          // (
-            if cfg.settings.schema_version < 23 then
-              {
-                bind_host = cfg.host;
-                bind_port = cfg.port;
-              }
-            else
-              { http.address = "${cfg.host}:${toString cfg.port}"; }
-          )
-        else
-          null;
-      configFile = (settingsFormat.generate "AdGuardHome.yaml" settings).overrideAttrs (_: {
-        checkPhase = "${cfg.package}/bin/adguardhome -c $out --check-config";
-      });
-    in
-    lib.mkForce (
-      lib.optionalString (settings != null) ''
-        if    [ -e "$STATE_DIRECTORY/AdGuardHome.yaml" ] \
-           && [ "${toString cfg.mutableSettings}" = "1" ]; then
-          # Writing directly to AdGuardHome.yaml results in empty file
-          ${pkgs.yaml-merge}/bin/yaml-merge "$STATE_DIRECTORY/AdGuardHome.yaml" "${configFile}" "${config.age.secrets.adguardhome.path}" > "$STATE_DIRECTORY/AdGuardHome.yaml.tmp"
-          mv "$STATE_DIRECTORY/AdGuardHome.yaml.tmp" "$STATE_DIRECTORY/AdGuardHome.yaml"
-        else
-          cp --force "${configFile}" "$STATE_DIRECTORY/AdGuardHome.yaml"
-          chmod 600 "$STATE_DIRECTORY/AdGuardHome.yaml"
-        fi
-      ''
-    );
 }
