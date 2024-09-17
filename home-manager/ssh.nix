79e41e6ddf0855c5cb0dce41a093701e4c8e799a
Andrew Lubawy <andrew@andrewlubawy.com>
2024-09-17 00:09:35 -0700

    Add bpi as host in ssh

--- home-manager/ssh.nix
+++ home-manager/ssh.nix
@@ -50,6 +50,19 @@
         identitiesOnly = true;
         identityFile = [ "~/.ssh/id_yubikey.pub" ];
       };
+      "bpi" = {
+        hostname = "192.168.1.1";
+        port = 22;
+        user = "${vars.user}";
+        extraOptions = {
+          ControlMaster = "no";
+          # FIXME: there is an issue with key length on the host when using a YubiKey with SSH.
+          # So we force it to ssh-ed25519, but the root cause should be fixed if possible.
+          HostKeyAlgorithms = "ssh-ed25519";
+        };
+        identitiesOnly = true;
+        identityFile = [ "~/.ssh/id_yubikey.pub" ];
+      };
     };
     serverAliveInterval = 300;
     serverAliveCountMax = 2;
