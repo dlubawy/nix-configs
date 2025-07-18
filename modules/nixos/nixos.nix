312ff456a29ceaf868297ab46b2182e2ac30f1d6
Andrew Lubawy <andrew@andrewlubawy.com>
2025-07-17 20:20:55 -0700

    fix: remove xdg.portals from nixos config

--- modules/nixos/nixos.nix
+++ modules/nixos/nixos.nix
@@ -63,10 +63,6 @@ in
     };
 
     services.userborn.enable = true;
-    xdg.portal = {
-      configPackages = with pkgs; [ xdg-desktop-portal-hyprland ];
-      wlr.enable = true;
-    };
 
     nix = {
       enable = true;
