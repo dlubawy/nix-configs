832ffd87f46d3a5de87a47c9de1f865b565f8b58
Andrew Lubawy <andrew@andrewlubawy.com>
2025-10-02 16:17:21 -0700

    fix: update age-plugin-yubikey

--- overlays/age-plugin-yubikey/default.nix
+++ overlays/age-plugin-yubikey/default.nix
@@ -6,15 +6,15 @@
     owner = "dlubawy";
     repo = "age-plugin-yubikey";
     rev = "rebase/combined_encryption";
-    sha256 = "sha256-UPdl705Y6k7spGhcWAV0u+dsnXistHPBueGfsZodrww=";
+    sha256 = "sha256-C1y0/LT9ERYiDGl0NIGDPTaRyD2EeiGqPHJWWKoNNxs=";
   };
   cargoHash = "";
   cargoDeps = prev.rustPlatform.importCargoLock {
     lockFile = ./Cargo.lock;
     outputHashes = {
       "age-core-0.10.0" = "sha256-Iw1KPYhUwfAvLGpYAGuSRhynrRJhD3EqOIS4UY6qC6c=";
-      "age-plugin-0.5.0" = "sha256-Iw1KPYhUwfAvLGpYAGuSRhynrRJhD3EqOIS4UY6qC6c=";
-      "yubikey-0.8.0" = "sha256-7HdETw+1PVFN6BwxfqilpCHvEC6BmtIqOVZf/CzScq8=";
+      "age-plugin-0.5.0" = "sha256-4wweIMpUSYqEZlXgAEjid7Th0put8gnl2wWF8/htuBw=";
+      "yubikey-0.8.0" = "sha256-4wweIMpUSYqEZlXgAEjid7Th0put8gnl2wWF8/htuBw=";
     };
   };
 }
