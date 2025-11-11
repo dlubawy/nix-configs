b55f4ae9b124b0572b418ed92ee62cffe6eeb10f
Andrew Lubawy <andrew@andrewlubawy.com>
2025-11-10 21:21:15 -0800

    feat: add initial password

--- users/drew.nix
+++ users/drew.nix
@@ -4,5 +4,6 @@
     email = "andrew@andrewlubawy.com";
     sshKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBz5R5RsCSXxzIduOV98T4yASCRbYrKVbzB7iZy9746P";
     gpgKey = "6EEC861B7641B37D";
+    initialHashedPassword = "$y$j9T$FsBDJelk43HF1v5gjR10o0$4pw56.HWiFvKGCovyZci42IL4j386JUeh1pj436ocF9";
   };
 }
