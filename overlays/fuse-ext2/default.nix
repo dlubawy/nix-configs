143821deef7c4d1502cb44bab67a9bc0570b7f49
Andrew Lubawy <andrew@andrewlubawy.com>
2025-11-18 09:09:21 -0800

    fix: don't overlay fuse packages with fuse-t when not using Darwin

--- overlays/fuse-ext2/default.nix
+++ overlays/fuse-ext2/default.nix
@@ -1,19 +1,29 @@
 { prev, final, ... }:
+let
+  isDarwin = prev.stdenv.hostPlatform.isDarwin;
+in
 {
-  src = prev.fetchFromGitHub {
-    owner = "macos-fuse-t";
-    repo = "fuse-ext2";
-    rev = "bef8e702749979fd7680a7b33a8e05d2a21da7f1";
-    hash = "sha256-RErvNiXWMB5lgqRVbDdNeJ7xQbmn4FVLTM2/Vk/jW6A=";
-  };
-  nativeBuildInputs = prev.fuse-ext2.nativeBuildInputs ++ [
-    prev.makeWrapper
-  ];
+  src =
+    if isDarwin then
+      prev.fetchFromGitHub {
+        owner = "macos-fuse-t";
+        repo = "fuse-ext2";
+        rev = "bef8e702749979fd7680a7b33a8e05d2a21da7f1";
+        hash = "sha256-RErvNiXWMB5lgqRVbDdNeJ7xQbmn4FVLTM2/Vk/jW6A=";
+      }
+    else
+      prev.src;
+  nativeBuildInputs =
+    prev.fuse-ext2.nativeBuildInputs
+    ++ (prev.lib.optionals isDarwin [
+      prev.makeWrapper
+    ]);
   buildInputs = [
     prev.e2fsprogs
-    final.fuse-t
-  ];
-  postInstall = ''
+  ]
+  ++ (prev.lib.optionals isDarwin [ final.fuse-t ])
+  ++ (prev.lib.optionals (!isDarwin) [ final.fuse ]);
+  postInstall = prev.lib.optionalString isDarwin ''
     wrapProgram $out/bin/fuse-ext2 --prefix DYLD_LIBRARY_PATH : "${final.fuse-t}/lib"
     wrapProgram $out/bin/fuse-ext2.probe --prefix DYLD_LIBRARY_PATH : "${final.fuse-t}/lib"
   '';
