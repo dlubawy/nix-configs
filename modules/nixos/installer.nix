687ceb6219823d240154976b93e5c7d3c538e909
Andrew Lubawy <andrew@andrewlubawy.com>
2025-11-18 09:11:26 -0800

    fix: don't enable nix gc in iso image

--- modules/nixos/installer.nix
+++ modules/nixos/installer.nix
@@ -1,16 +1,20 @@
 {
   pkgs,
+  lib,
   inputs,
-  outputs,
-  vars,
   modulesPath,
   ...
 }:
+let
+  inherit (lib) mkForce;
+in
 {
   imports = [
     (modulesPath + "/installer/cd-dvd/installation-cd-minimal.nix")
     ./nix.nix
   ];
+
+  nix.gc.automatic = mkForce false;
 
   environment.systemPackages = [
     inputs.disko.packages.${pkgs.system}.disko
