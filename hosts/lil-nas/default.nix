ef07f86af5606307f40cdad558e7459b6860efa0
Andrew Lubawy <andrew@andrewlubawy.com>
2025-11-22 20:26:26 -0800

    feat: add topology for nas

--- hosts/lil-nas/default.nix
+++ hosts/lil-nas/default.nix
@@ -5,9 +5,11 @@
   imports = [
     ./disko.nix
     ./hardware.nix
+    ./topology.nix
     ../../users/default.nix
   ];
 
+  networking.hostName = "lil-nas";
   home-manager.gui.enable = false;
   users.shadow.enable = true;
 }
---
+++ hosts/lil-nas/topology.nix
@@ -0,0 +1,20 @@
+{ config, ... }:
+let
+  inherit (config.lib.topology) mkConnectionRev;
+in
+{
+  topology = {
+    enable = true;
+    self = {
+      hardware.info = "GMKtec G9";
+      interfaces = {
+        lan1 = {
+          addresses = [ "192.168.1.11" ];
+          physicalConnections = [
+            (mkConnectionRev "bpi" "lan1")
+          ];
+        };
+      };
+    };
+  };
+}
