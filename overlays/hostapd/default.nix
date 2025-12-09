1f1caec05e1c6168140226c61475b85b5f38fbf9
Andrew Lubawy <andrew@andrewlubawy.com>
2025-12-09 13:53:33 -0800

    fix: try patching with VLAN PMKSA fixes

--- overlays/hostapd/default.nix
+++ overlays/hostapd/default.nix
@@ -5,11 +5,6 @@
   # Authentication will not work in those instances with disable_pmksa_caching=1
   # Having an empty cache list on each get call has been the only fix I have found so far
   patches = prev.hostapd.patches ++ [
-    # patches out PMKSA without disabling PMKSA; needed for 5G and Apple devices
-    ./null_pmksa_cache.patch
-    # reorders the vlan logic in an attempt to get dynamic vlan working with caching
-    ./wildcard_vlan.patch
-    # adds vlan description struct to the cache entry when EAPOL data is not available
-    ./pmksa_vlan.patch
+    ./pmksa_vlan_fixes.patch
   ];
 }
