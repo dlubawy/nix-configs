47ba8823b62d9598cb961028b047366e8eb5308c
Andrew Lubawy <12501720+dlubawy@users.noreply.github.com>
2025-12-11 17:54:08 -0800

    fix/enable-pmksa-caching (#138)

--- overlays/hostapd/default.nix
+++ overlays/hostapd/default.nix
@@ -1,15 +1,9 @@
 { prev, ... }:
 {
-  # NOTE: null_pmksa_cache.patch is required because PMKSA is broken with dynamic VLAN
-  # PMKSA is still needed for 5G and Apple devices to work
-  # Authentication will not work in those instances with disable_pmksa_caching=1
-  # Having an empty cache list on each get call has been the only fix I have found so far
-  patches = prev.hostapd.patches ++ [
-    # patches out PMKSA without disabling PMKSA; needed for 5G and Apple devices
-    ./null_pmksa_cache.patch
-    # reorders the vlan logic in an attempt to get dynamic vlan working with caching
-    ./wildcard_vlan.patch
-    # adds vlan description struct to the cache entry when EAPOL data is not available
-    ./pmksa_vlan.patch
-  ];
+  # NOTE: Fixes PMKSA issues with dynamic VLAN and SAE auth
+  src = prev.fetchgit {
+    url = "https://git.w1.fi/hostap.git";
+    rev = "5d0f442c0ce67b7afacc10a512c09fedc2c75192";
+    hash = "sha256-5HG/0WZPR5377Mfh2ao9cvemWsyhNuJDxQrk1MibCls=";
+  };
 }
