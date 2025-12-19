91853ed5cc5d09b4a1874825f3409d610ccc16b7
Andrew Lubawy <andrew@andrewlubawy.com>
2025-12-19 10:41:38 -0800

    fix: jellyfin srv dir permissions

--- hosts/lil-nas/jellyfin.nix
+++ hosts/lil-nas/jellyfin.nix
@@ -7,17 +7,26 @@ let
   inherit (lib) mkIf;
 in
 {
-  systemd.services = {
-    jellyfin.environment.LIBVA_DRIVER_NAME = "iHD";
-    jellyfin-taildrive = mkIf config.services.tailscale.enable {
-      description = "Tailscale drive service";
-      after = [ "tailscale.service" ];
-      serviceConfig = {
-        User = "jellyfin";
-        Group = "jellyfin";
-        Type = "oneshot";
-        RemainAfterExit = true;
-        ExecStart = "${config.services.tailscale.package}/bin/tailscale drive share jellyfin /srv/jellyfin";
+  systemd = {
+    services = {
+      jellyfin.environment.LIBVA_DRIVER_NAME = "iHD";
+      jellyfin-taildrive = mkIf config.services.tailscale.enable {
+        description = "Tailscale drive service";
+        after = [ "tailscale.service" ];
+        serviceConfig = {
+          User = "jellyfin";
+          Group = "jellyfin";
+          Type = "oneshot";
+          RemainAfterExit = true;
+          ExecStart = "${config.services.tailscale.package}/bin/tailscale drive share jellyfin /srv/jellyfin";
+        };
+      };
+    };
+    tmpfiles.settings.jellyfin."/srv/jellyfin" = {
+      Z = {
+        mode = "755";
+        user = config.services.jellyfin.user;
+        group = config.services.jellyfin.group;
       };
     };
   };
