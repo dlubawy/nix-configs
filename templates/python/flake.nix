eee3416e0dae64ff20ca16e5519cd740a686a782
Andrew Lubawy <andrew@andrewlubawy.com>
2026-05-30 17:34:00 -0700

    chore: sync template flakes

--- templates/python/flake.nix
+++ templates/python/flake.nix
@@ -1,7 +1,7 @@
 {
   description = "A Nix flake based Python environment";
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
 
     # Python inputs
     pyproject-nix = {
