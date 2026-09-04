84cd49247d95ee55520fe2a44ec76c24c0104276
Andrew Lubawy <andrew@andrewlubawy.com>
2026-09-04 09:53:25 -0700

    fix: update forked age branch

--- overlays/age/default.nix
+++ overlays/age/default.nix
@@ -4,6 +4,6 @@
     owner = "dlubawy";
     repo = "age";
     rev = "feat/x25519-tag-tagpq-support";
-    hash = "sha256-U/u4AXKp9RfVRzFYNOT/oeOikIlyhOSVfxcXK8gNDWs=";
+    hash = "sha256-Tc4hrWeKbdlR1dbSM3LaKoqyKYl4ScYq8l7/n2jhl1c=";
   };
 }
